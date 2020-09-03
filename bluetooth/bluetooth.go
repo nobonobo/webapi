401ce576e256229251bf333f876b74b126ab7704
Irieda Noboru <irieda@144lab.com>
2020-09-03 20:38:02 +0900

    fix bluetooth

--- bluetooth/bluetooth.go
+++ bluetooth/bluetooth.go
@@ -4,27 +4,11 @@ import (
 	"github.com/nobonobo/spago/jsutil"
 )
 
-// Bluetooth ...
-type Bluetooth struct {
-	devices []*Device
-}
-
 // RequestDevice ...
-func (bt *Bluetooth) RequestDevice(filter map[string]interface{}) ([]*Device, error) {
+func RequestDevice(filter map[string]interface{}) (*Device, error) {
 	device, err := jsutil.Await(bluetooth.Call("requestDevice", filter))
 	if err != nil {
 		return nil, err
 	}
-	res := []*Device{}
-	for i := 0; i < device.Length(); i++ {
-		res = append(res, &Device{Value: device.Index(i)})
-	}
-	return res, nil
-}
-
-// Release ...
-func (bt *Bluetooth) Release() {
-	for _, d := range bt.devices {
-		d.Release()
-	}
+	return &Device{Value: device}, nil
 }
